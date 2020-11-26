98211be1a580cb9502ee4d36e85664bdd0e01d40
= <=>
2020-11-26 12:42:34 -0500

    React Router added

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,11 +11,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/checkout' component={Checkout} />
-        <Route exact path='/checkout/information' component={Information} />
-        <Route exact path='/checkout/payment' component={Payment} />
-        <Route exact path='/checkout/success' component={Success} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={Success} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
