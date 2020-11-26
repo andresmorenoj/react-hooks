324d88e3a388b95c098bd1550ee5dfa694f32a39
= <=>
2020-11-26 12:12:24 -0500

    Testing Husky

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,9 @@
-import React from "react";
-
+import React from 'react';
 
 import '../assets/styles/components/App.scss';
 
-
-
-
 const App = () => {
-
   return <h1>Hola, Mundo!</h1>;
-
-
-
-
-
 };
-
-
-
-
-
-
-
 
 export default App;
