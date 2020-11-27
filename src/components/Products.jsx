e2515150883c1f08824fc3b8f407fef14d981869
= <=>
2020-11-27 18:51:12 -0500

    React Context connected to Header, Protucts and Checkout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
+import AppContext from '../context/AppContext';
 import '../assets/styles/components/Products.scss';
 
-const Products = ({ products }) => {
+const Products = () => {
+  // @ts-ignore
+  const { state, addToCart } = useContext(AppContext);
+  const { products } = state;
+
+  const handleAddToCard = (product) => () => {
+    addToCart(product);
+  };
   return (
     <div className="Products">
       <div className="Products-items">
         {products.map((product) => (
-          <Product key={product.id} product={product} />
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCard={handleAddToCard}
+          />
         ))}
       </div>
     </div>
