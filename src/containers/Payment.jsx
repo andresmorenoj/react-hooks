cc332e4d0ecc6a2782b1dcbdcef3bc8b046ffd31
= <=>
2020-11-28 08:01:30 -0500

    PayPayl payment added

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import AppContext from '../context/AppContext';
+import { PayPalButton } from 'react-paypal-button';
 import '../assets/styles/containers/Payment.scss';
 
 const Payment = () => {
+  // @ts-ignore
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+  const history = useHistory();
+
+  const paypalOptions = {
+    clientId:
+      'AfQO5c_l75tgyIQ7AdO7fo8X0HVDtB1Udfr3t6-KtCBrYglxt_t6v9riWHAcQa4_2mDvQ61UXNOECe4b',
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect',
+  };
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === 'COMPLITED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      history.push('/checkout/success');
+    }
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido:</h3>
-        <div className="Payment-button">Botón de pago con PayPal</div>
+        {cart.map((item) => (
+          <div className="Payment item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PayPalButton
+            // @ts-ignore
+            paypalOptions={paypalOptions}
+            // @ts-ignore
+            buttonStyles={buttonStyles}
+            // @ts-ignore
+            amount={handleSumTotal}
+            onPaymentStart={() => console.log('Start Payment')}
+            onPaymentSuccess={(data) => handlePaymentSuccess(data)}
+            onPaymentError={(error) => console.log(error)}
+            onPaymentCancel={(data) => console.log(data)}
+          />
+        </div>
       </div>
     </div>
   );
