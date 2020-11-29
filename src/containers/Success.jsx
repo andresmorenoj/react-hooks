e8d83f653fcb6dec8f46d970b4b6d768e298dfbd
= <=>
2020-11-29 12:18:08 -0500

    Custom hook for maps added

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,19 +1,22 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../assets/styles/containers/Success.scss';
 
 const Success = () => {
   // @ts-ignore
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer[0].address);
+
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección:</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
